9a705b4d81fbfdb404012feb93752556ae3a7405
Alexi <jemimassyrup@gmail.com>
2024-02-20 18:51:20 -0800

    Contact

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -163,8 +163,12 @@ const Contact = () => {
 			</div>
 			</div>
 		)}
-		<p className="mb-4 text-lg font-medium text-dark dark:text-white">Or you can email us directly at 
-		<a  href="mailto:devolympus@hotmail.com" className=" text-center text-primary">devolympus@hotmail.com</a></p>
+		<Animate className="flex justify-center ">
+			<p className="mb-4 text-lg font-medium text-dark dark:text-white">
+				Or you can email us directly at
+				<p  className="inline text-primary text-lg font-medium"> devolympus@hotmail.com</p>
+			</p>
+		</Animate>
     </section>
   );
 };
