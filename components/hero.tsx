e7fd7b5a1daff6d028ff639432e570cb01d36b60
alexiosiko <jemimassyrup@gmail.com>
2025-08-18 15:59:41 +0300

    fixed hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -23,50 +23,52 @@ export default function Hero() {
 
 
   return (
-    <section className="h-screen flex-col justify-center  flex">
-      {/* Logo/Video */}
-	  <div >
+    	<section className="h-screen relative flex-col justify-center flex">
+		{/* Logo/Video */}
+	 	 <div className=' relative min-h-[460px] h-min max-w-2xl  mx-auto' >
 
-       {fallback ? 
-			<motion.img
-			src="/logo/image.png"
-			alt="bird-box-studios.png"
-			className="h-full w-full max-w-xl mx-auto object-contain"
-			height={400}
-			width={400}
-			initial={{ opacity: 0, scale: 0.8 }}
-			animate={{ opacity: 1, scale: 1 }}
-			transition={{ duration: 0.8, ease: 'easeOut' }}
-			/> :
-			<video
-			ref={videoRef}
-			muted
-			autoPlay
-			playsInline
-			className="h-full w-full max-w-xl mx-auto object-contain"
+			{fallback ? 
+					<motion.img
+					src="/logo/image.png"
+					alt="bird-box-studios.png"
+					className=" w-full object-contain"
+					height={500}
+					width={400}
+					initial={{ opacity: 0, scale: 0.8 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{ duration: 0.8, ease: 'easeOut' }}
+					/> :
+					<video
+					ref={videoRef}
+					height={500}
+					width={400}
+					muted
+					autoPlay
+					playsInline
+					className=" w-full object-contain"
+					>
+					<source src="/logo/anim_1024_compressed.mp4" type="video/mp4" />
+					<source src="/logo/anim_1024_compressed.webm" type="video/webm" />
+					</video>   
+				}
+
+			<motion.div className='flex justify-center relative -top-[20%]'
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ delay: 1.5}}
 			>
-			<source src="/logo/anim.mp4" type="video/mp4" />
-			<source src="/logo/anim.webm" type="video/webm" />
-			</video>   
-		}
 
+				<Button 
+					onClick={() => document.getElementById('games')?.scrollIntoView({ behavior: 'smooth' })}
+					className="text-lg md:text-xl px-10"
+					>
+					See Our Games
+				</Button>
+			</motion.div>
 	</div>
       {/* Bottom 1/3: Animated Text + Button */}
 		
 
-	  <motion.div className=' mx-auto relative bottom-[10%]'
-	initial={{ opacity: 0, y: 20 }}
-	animate={{ opacity: 1, y: 0 }}
-	  transition={{ delay: fallback ? 1.25 : 0}}
-	  >
-
-		<Button 
-			onClick={() => document.getElementById('games')?.scrollIntoView({ behavior: 'smooth' })}
-			className="text-lg md:text-xl px-10"
-			>
-			See Our Games
-		</Button>
-			</motion.div>
        
 
     </section>
