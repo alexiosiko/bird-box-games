5485e6d6a26d48d3bf1a48dd8cb93d4fef59cde0
alexiosiko <jemimassyrup@gmail.com>
2025-10-19 10:08:12 -0700

    platofmr

---
+++ app/route/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function GamePlatforms({ params 
+}: { params: Promise<{ id: string }> }) {
+
+	return (
+		<div className="flex flex-col items-center gap-3">
+
+		</div>
+	);
+}
